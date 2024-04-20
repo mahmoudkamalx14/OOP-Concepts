713e16b9510dfcd4b917d9e4a72169d1ea45eb3c
Mahmoud Kamal <213179+mahmoudkamalx17@users.noreply.github.com>
2024-04-20 04:49:53 +0200

    add main file

---
+++ main.dart
@@ -0,0 +1,26 @@
+void main() {
+  print('# OOP Concepts');
+}
+
+
+//! # Object-Oriented Programming (OOP) Concepts
+
+// This repository is dedicated to exploring and understanding various concepts
+// in Object-Oriented Programming (OOP).
+
+// ## What is Object-Oriented Programming (OOP)?
+
+// Object-Oriented Programming (OOP) is a programming paradigm based on 
+// the concept of "objects", which can contain data and code: 
+// data in the form of fields (often known as attributes or properties),
+// and code, in the form of procedures (often known as methods).
+
+//? ## Contents
+
+//* 1. [Encapsulation](encapsulation.md)
+//! 2. [Inheritance](inheritance.md)
+//? 3. [Polymorphism](polymorphism.md)
+//* 4. [Abstraction](abstraction.md)
+//! 5. [Composition](composition.md)
+
+// Each concept has its own document explaining its definition, importance, and practical examples.
